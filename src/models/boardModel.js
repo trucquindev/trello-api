942203a9a9cd991ec57a4049cffc4eeeffcdf011
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-09-29 17:41:57 +0700

    code base model name and schema

---
+++ src/models/boardModel.js
@@ -0,0 +1,21 @@
+import Joi from 'joi'
+import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '~/utils/validators'
+
+//define collection (schema and name)
+
+const BOARD_COLLECTION_NAME = 'boards'
+const BOARD_COLLECTION_SCHEMA = Joi.object({
+  title: Joi.string().required().min(3).max(50).trim().strict(),
+  slug: Joi.string().required().min(2).trim().strict(),
+  description: Joi.string().required().min(3).max(256).trim().strict(),
+
+  columnOrderIds: Joi.array().items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE)).default([]),
+  createdAt: Joi.date().timestamp('javascript').default(Date.now),
+  updatedAt: Joi.date().timestamp('javascript').default(null),
+  _destroy: Joi.boolean.default(false)
+})
+
+export const boardModel= {
+  BOARD_COLLECTION_NAME,
+  BOARD_COLLECTION_SCHEMA
+}
