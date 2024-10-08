4eb4fe6bdfa1206a7339cb60c02a1154ecf29bad
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-10-08 23:12:56 +0700

    add whiteList Domains

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,6 @@
 export const WHITELIST_DOMAINS = [
   // 'http://localhost:5173' // khong can localhost nua
+  'https://trello-web-2024.vercel.app'
 ]
 export const BOARD_TYPE = {
   PUBLIC: 'public',
