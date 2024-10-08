aaf44650a5ea11d8eb0607cb4b71d09c1f135b81
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-10-08 22:22:27 +0700

    Update CORS not allow localhost

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
-export const WHITELIST_DOMAINS = 'http://localhost:5173'
+export const WHITELIST_DOMAINS = [
+  // 'http://localhost:5173' // khong can localhost nua
+]
 export const BOARD_TYPE = {
   PUBLIC: 'public',
   PRIVATE: 'private'
