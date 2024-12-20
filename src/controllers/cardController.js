081d171dc636b205ec3c3b8880a609a3845a22b0
quynhdeptrai306 <nguyentrucquynh3006@gmail.com>
2024-12-20 16:16:01 +0700

    feat(api-update-card): create api update card include cover, title, des, ..

--- src/controllers/cardController.js
+++ src/controllers/cardController.js
@@ -1,17 +1,34 @@
-
-import { StatusCodes } from 'http-status-codes'
-import ApiError from '~/utils/ApiError'
-import { cardService } from '~/services/cardService'
-const createNew = async(req, res, next) => {
+import { StatusCodes } from 'http-status-codes';
+import { cardService } from '~/services/cardService';
+const createNew = async (req, res, next) => {
   try {
-
     // điều hướng dữ liệu sang service
-    const creatNewCard = await cardService.createNew(req.body)
+    const creatNewCard = await cardService.createNew(req.body);
 
     // trả về kết quả thành công
-    res.status(StatusCodes.CREATED).json(creatNewCard)
-  } catch (error) { next(error) }
-}
+    res.status(StatusCodes.CREATED).json(creatNewCard);
+  } catch (error) {
+    next(error);
+  }
+};
+const update = async (req, res, next) => {
+  try {
+    const cardId = req.params.id;
+    const cardCoverFile = req.file;
+    // điều hướng dữ liệu sang service
+    const updateCard = await cardService.update(
+      cardId,
+      req.body,
+      cardCoverFile
+    );
+
+    // trả về kết quả thành công
+    res.status(StatusCodes.OK).json(updateCard);
+  } catch (error) {
+    next(error);
+  }
+};
 export const cardController = {
-  createNew
-}
+  createNew,
+  update,
+};
--- src/models/cardModel.js
+++ src/models/cardModel.js
@@ -1,5 +1,10 @@
 import Joi from 'joi';
-import { OBJECT_ID_RULE, OBJECT_ID_RULE_MESSAGE } from '../utils/validators';
+import {
+  OBJECT_ID_RULE,
+  OBJECT_ID_RULE_MESSAGE,
+  EMAIL_RULE,
+  EMAIL_RULE_MESSAGE,
+} from '../utils/validators';
 import { GET_DB } from '~/config/mongodb';
 import { ObjectId } from 'mongodb';
 // Define Collection (name & schema)
@@ -16,7 +21,23 @@ const CARD_COLLECTION_SCHEMA = Joi.object({
 
   title: Joi.string().required().min(3).max(50).trim().strict(),
   description: Joi.string().optional(),
-
+  cover: Joi.string().default(null),
+  memberIds: Joi.array()
+    .items(Joi.string().pattern(OBJECT_ID_RULE).message(OBJECT_ID_RULE_MESSAGE))
+    .default([]),
+  // NHUNG DU LIEU COMMENT CUA CARD DUNG EMBEDDED VAO BANG GHI CARD
+  comments: Joi.array()
+    .items({
+      userId: Joi.string()
+        .pattern(OBJECT_ID_RULE)
+        .message(OBJECT_ID_RULE_MESSAGE),
+      userEmail: Joi.string().pattern(EMAIL_RULE).message(EMAIL_RULE_MESSAGE),
+      userAvatar: Joi.string(),
+      userDisplayName: Joi.string(),
+      content: Joi.string(),
+      commentedAt: Joi.date().timestamp(),
+    })
+    .default([]),
   createdAt: Joi.date().timestamp('javascript').default(Date.now),
   updatedAt: Joi.date().timestamp('javascript').default(null),
   _destroy: Joi.boolean().default(false),
